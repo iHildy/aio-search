4036b677495eb7211770605ff0519d600011e512
Ian Hildebrand <ian@jedi.net>
2020-09-20 21:40:24 -0500

    Added discord webhook support
    just for fun lol, still 2 major bugs that should be fixed soon

--- AutoLookUp.py
+++ AutoLookUp.py
@@ -2,6 +2,8 @@ from subprocess import call
 import re
 import sys
 import webbrowser
+from dhooks import Webhook, Embed
+
 
 print("""\
 
@@ -17,6 +19,43 @@ print("""\
 links = []
 
 filename = 'C:\\Users\\TestoW\\Documents\\Coding\\definition-search\\searches.txt'
+hook = Webhook('')
+if hook == '':
+    hook = input("What's the webhook? ")
+else:
+    print("Webhook detected")
+
+
+def successhook():
+
+    embed = Embed(
+        title='AIO Search Executed Successfully',
+        description="AIO Search Program was able to open you're defined searches",
+        color=0x00ff1e,
+        timestamp='now'  # sets the timestamp to current time
+    )
+
+    image1 = 'https://avatars2.githubusercontent.com/u/25069719?s=460&u=0758922d6a85a09f971fbf778bb720788a2f2e5b&v=4'
+
+    embed.set_footer(text='AIO Search | Made by iHildy#3839', icon_url=image1)
+
+    hook.send(embed=embed)
+
+def badhook():
+    embed = Embed(
+        title='AIO Search Failed',
+        description="AIO Search Program was un-able to open you're defined searches",
+        color=0xff0000,
+        timestamp='now'  # sets the timestamp to current time
+    )
+ 
+    image1 = 'https://avatars2.githubusercontent.com/u/25069719?s=460&u=0758922d6a85a09f971fbf778bb720788a2f2e5b&v=4'
+
+    embed.add_field(name='Test Field', value='Value of the field :open_mouth:')
+    embed.add_field(name='Another Field', value='1234 :smile:')
+    embed.set_footer(text='AIO Search | Made by iHildy#3839', icon_url=image1)
+
+    hook.send(embed=embed)
 
 def aiosearch():
     searchtype = input(
@@ -28,27 +67,39 @@ def aiosearch():
                 if link != '':
                     if re.match('http://.+|https://.+|ftp://.+|file://.+', link.lower()):
                         links.append(link)
+                        successhook()
                     elif searchtype == "1":
                         links.append('https://www.google.com/search?q=' + link)
+                        successhook()
                     elif searchtype == "2":
                         subject = input("What subject/class is this for? ")
                         links.append('https://www.google.com/search?q=' + link + ' ' + subject + ' definition')
+                        successhook()
                     elif searchtype == "3":
                         links.append('http://maps.google.com/?q=' + link)
+                        successhook()
                     elif searchtype == "4":
                         links.append('https://www.youtube.com/results?search_query=' + link)
+                        successhook()
                     elif searchtype == "5":
                         links.append('https://github.com/search?q=' + link)
+                        successhook()
                     elif searchtype == "6":
                         links.append('https://stackoverflow.com/search?q=' + link)
+                        successhook()
                     else:
                         print(
-                            "Something went wrong, make sure you filled the searches.txt file, and gave the program the correct path. inside")
+                            "Something went wrong, you might not have typed the number in right.")
+                        badhook()
+
     except IOError:
-        print('Failed to open the file "%s".\nExiting.')
+        print('Something went wrong, make sure you filled the searches.txt file correctly, and gave the program the correct path inside')
         sys.exit()
     print(links)
     for lnk in links:
         webbrowser.open_new_tab(lnk)
+
+
+
 
 aiosearch()
